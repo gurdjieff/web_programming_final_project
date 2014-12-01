d7d14cca4f7b3df23e2a263d7763f9e1a4a4ce23
DaiyuZhang <gurdjieff100@gmail.com>
2014-12-01 13:34:10 +0000

    1/12/2014

--- db/migrate/20141116014310_create_course_subject_join_table.rb
+++ db/migrate/20141116014310_create_course_subject_join_table.rb
@@ -1,8 +1,8 @@
 class CreateCourseSubjectJoinTable < ActiveRecord::Migration
   def change
   	create_table :courses_subjects, :id => false do |t|
-          t.integer :course_id
-          t.integer :subject_id
+      t.integer :course_id
+      t.integer :subject_id
     end
   end
 end
